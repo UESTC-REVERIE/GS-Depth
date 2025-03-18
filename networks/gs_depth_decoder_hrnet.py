be3905365b4254a5d6e9b9b54932e3daf8173575
reverie@172.16.4.4 <reverie.phm@outlook.com>
2025-03-18 16:03:36 +0800

    添加重构的hrnet_decoder

--- networks/gs_depth_decoder_hrnet.py
+++ networks/gs_depth_decoder_hrnet.py
@@ -19,38 +19,54 @@ class GSDepthDecoder(nn.Module):
         self.scales = scales
 
         self.num_ch_enc = num_ch_enc  # np.array([64, 18, 36, 72, 144])
-        # self.num_ch_dec = np.array([16, 32, 64, 128])
+        # decoder stage 0-6 number of channels
+        self.num_ch_dec = np.array([32, 64, 18, 36, 72, 144])
         self.use_skips = use_skips
 
         self.convs = OrderedDict()
         
-        self.convs[("parallel_conv"), 0, 4] = ConvBlock(self.num_ch_enc[4], self.num_ch_enc[4])
-        self.convs[("conv1x1", 0, 2_1)] = ConvBlock1x1(self.num_ch_enc[2], self.num_ch_enc[1])
-        self.convs[("conv1x1", 0, 3_2)] = ConvBlock1x1(self.num_ch_enc[3], self.num_ch_enc[2])
-        self.convs[("conv1x1", 0, 3_1)] = ConvBlock1x1(self.num_ch_enc[3], self.num_ch_enc[1])
-        self.convs[("conv1x1", 0, 4_3)] = ConvBlock1x1(self.num_ch_enc[4], self.num_ch_enc[3])
-        self.convs[("conv1x1", 0, 4_2)] = ConvBlock1x1(self.num_ch_enc[4], self.num_ch_enc[2])
-        self.convs[("conv1x1", 0, 4_1)] = ConvBlock1x1(self.num_ch_enc[4], self.num_ch_enc[1])
-
-        self.convs[("parallel_conv"), 1, 1] = ConvBlock(self.num_ch_enc[1], self.num_ch_enc[1])
-        self.convs[("parallel_conv"), 1, 2] = ConvBlock(self.num_ch_enc[2], self.num_ch_enc[2])
-        self.convs[("parallel_conv"), 1, 3] = ConvBlock(self.num_ch_enc[3], self.num_ch_enc[3])
-        self.convs[("conv1x1", 1, 2_1)] = ConvBlock1x1(self.num_ch_enc[2], self.num_ch_enc[1])
-        self.convs[("conv1x1", 1, 3_2)] = ConvBlock1x1(self.num_ch_enc[3], self.num_ch_enc[2])
-        self.convs[("conv1x1", 1, 3_1)] = ConvBlock1x1(self.num_ch_enc[3], self.num_ch_enc[1])
-
-        self.convs[("parallel_conv"), 2, 1] = ConvBlock(self.num_ch_enc[1], self.num_ch_enc[1])
-        self.convs[("parallel_conv"), 2, 2] = ConvBlock(self.num_ch_enc[2], self.num_ch_enc[2])
-        self.convs[("conv1x1", 2, 2_1)] = ConvBlock1x1(self.num_ch_enc[2], self.num_ch_enc[1])
-
-        self.convs[("parallel_conv"), 3, 0] = ConvBlock(self.num_ch_enc[0], self.num_ch_enc[0])
-        self.convs[("parallel_conv"), 3, 1] = ConvBlock(self.num_ch_enc[1], self.num_ch_enc[1])
-        self.convs[("conv1x1", 3, 1_0)] = ConvBlock1x1(self.num_ch_enc[1], self.num_ch_enc[0])
-
-        self.convs[("parallel_conv"), 4, 0] = ConvBlock(self.num_ch_enc[0], 32)
-        self.convs[("parallel_conv"), 5, 0] = ConvBlock(32, 16)
-        self.convs[("dispconv", 0)] = Conv3x3(16, self.num_output_channels)
-
+        # ("parallel_conv", x, y) stage(scale) x, index y
+        # stage_4 scale 1/16 
+        stage_5_ch_num = self.num_ch_dec[5]
+        stage_4_ch_num = self.num_ch_dec[4]
+        self.convs[("parallel_conv", 4, 0)] = ConvBlock(stage_4_ch_num, stage_4_ch_num)
+        stage_3_ch_num = self.num_ch_dec[3]
+        self.convs[("parallel_conv", 3, 0)] = ConvBlock(stage_3_ch_num, stage_3_ch_num)
+        self.convs[("parallel_conv", 3, 1)] = ConvBlock(stage_3_ch_num, stage_3_ch_num)
+        stage_2_ch_num = self.num_ch_dec[2]
+        self.convs[("parallel_conv", 2, 0)] = ConvBlock(stage_2_ch_num, stage_2_ch_num)
+        self.convs[("parallel_conv", 2, 1)] = ConvBlock(stage_2_ch_num, stage_2_ch_num)
+        self.convs[("parallel_conv", 2, 2)] = ConvBlock(stage_2_ch_num, stage_2_ch_num)
+        stage_1_ch_num = self.num_ch_dec[1]
+        self.convs[("parallel_conv", 1, 0)] = ConvBlock(stage_1_ch_num, stage_1_ch_num)
+        # self.convs[("parallel_conv", 1, 1)] = ConvBlock(stage_1_ch_num, stage_0_ch_num)
+        stage_0_ch_num = self.num_ch_dec[0]
+        self.convs[("parallel_conv", 0, 0)] = ConvBlock(stage_0_ch_num, stage_0_ch_num)
+        
+        # ("conv1x1", x, y, z) 
+        # upsampled feature conv for declining channels: No.z frome stage x to stage y
+        # stage 5
+        self.convs[("conv1x1", 5, 4, 0)] = ConvBlock1x1(stage_5_ch_num, stage_4_ch_num)
+        self.convs[("conv1x1", 5, 3, 0)] = ConvBlock1x1(stage_5_ch_num, stage_3_ch_num)
+        self.convs[("conv1x1", 5, 2, 0)] = ConvBlock1x1(stage_5_ch_num, stage_2_ch_num)
+        # stage 4
+        self.convs[("conv1x1", 4, 3, 0)] = ConvBlock1x1(stage_4_ch_num, stage_3_ch_num)
+        self.convs[("conv1x1", 4, 3, 1)] = ConvBlock1x1(stage_4_ch_num, stage_3_ch_num)
+        self.convs[("conv1x1", 4, 2, 0)] = ConvBlock1x1(stage_4_ch_num, stage_2_ch_num)
+        self.convs[("conv1x1", 4, 2, 1)] = ConvBlock1x1(stage_4_ch_num, stage_2_ch_num)
+        # stage 3
+        self.convs[("conv1x1", 3, 2, 0)] = ConvBlock1x1(stage_3_ch_num, stage_2_ch_num)
+        self.convs[("conv1x1", 3, 2, 1)] = ConvBlock1x1(stage_3_ch_num, stage_2_ch_num)
+        self.convs[("conv1x1", 3, 2, 2)] = ConvBlock1x1(stage_3_ch_num, stage_2_ch_num)
+        # stage 2
+        self.convs[("conv1x1", 2, 1, 0)] = ConvBlock1x1(stage_2_ch_num, stage_1_ch_num)
+        # stage 1
+        self.convs[("conv1x1", 1, 0, 0)] = ConvBlock1x1(stage_1_ch_num, stage_0_ch_num)
+        
+        self.convs[("dispconv", 0)] = nn.Sequential(
+            Conv3x3(stage_0_ch_num, stage_0_ch_num // 2),
+            Conv3x3(stage_0_ch_num // 2, self.num_output_channels),
+        )
 
         self.decoder = nn.ModuleList(list(self.convs.values()))
         self.sigmoid = nn.Sigmoid()
@@ -60,66 +76,65 @@ class GSDepthDecoder(nn.Module):
     def forward(self, input_features):
         self.outputs = {}
         
-        d0_0 = input_features[0]
-        d0_1 = input_features[1]
-        d0_2 = input_features[2]
-        d0_3 = input_features[3]
-        d0_4 = self.convs[("parallel_conv"), 0, 4](input_features[4])
+        d1_0 = input_features[0]
+        d2_0 = input_features[1]
+        d3_0 = input_features[2]
+        d4_0 = input_features[3]
+        d5_0 = input_features[4]
+
+        d3to2_0 = updown_sample(d3_0, 2)
+        d4to3_0 = updown_sample(d4_0, 2)
+        d4to2_0 = updown_sample(d4_0, 4)
+        d5to4_0 = updown_sample(d5_0, 2)
+        d5to3_0 = updown_sample(d5_0, 4)
+        d5to2_0 = updown_sample(d5_0, 8)
+
+        d5to4_0 = self.convs[("conv1x1", 5, 4, 0)](d5to4_0)
+        d5to3_0 = self.convs[("conv1x1", 5, 3, 0)](d5to3_0)
+        d5to2_0 = self.convs[("conv1x1", 5, 2, 0)](d5to2_0)
+        d4to3_0 = self.convs[("conv1x1", 4, 3, 0)](d4to3_0)
+        d4to2_0 = self.convs[("conv1x1", 4, 2, 0)](d4to2_0)
+        d3to2_0 = self.convs[("conv1x1", 3, 2, 0)](d3to2_0)
+
+        d2_msf_0 = d5to2_0 + d4to2_0 + d3to2_0 + d2_0
+        d3_msf_0 = d5to3_0 + d4to3_0 + d3_0
+        d4_msf_0 = d5to4_0 + d4_0
+
+
+        d4_1 = self.convs[("parallel_conv", 4, 0)](d4_msf_0)
+        d3_1 = self.convs[("parallel_conv", 3, 1)](d3_msf_0)
+        d2_1 = self.convs[("parallel_conv", 2, 1)](d2_msf_0)
+
+        d3to2_1 = updown_sample(d3_1, 2)
+        d4to3_1 = updown_sample(d4_1, 2)
+        d4to2_1 = updown_sample(d4_1, 4)
+
+        d3to2_1 = self.convs[("conv1x1", 3, 2, 1)](d3to2_1)
+        d4to3_1 = self.convs[("conv1x1", 4, 3, 1)](d4to3_1)
+        d4to2_1 = self.convs[("conv1x1", 4, 2, 1)](d4to2_1)
+
+        d2_msf_1 = d3to2_1 + d4to2_1 + d2_1
+        d3_msf_1 = d4to3_1 + d3_1
+
+        d2_2 = self.convs[("parallel_conv", 2, 1)](d2_msf_1)
+        d3_2 = self.convs[("parallel_conv", 3, 1)](d3_msf_1)
         
-        d0_2_1 = updown_sample(d0_2, 2)
-        d0_3_2 = updown_sample(d0_3, 2)
-        d0_3_1 = updown_sample(d0_3, 4)
-        d0_4_3 = updown_sample(d0_4, 2)
-        d0_4_2 = updown_sample(d0_4, 4)
-        d0_4_1 = updown_sample(d0_4, 8)
+        d3to2_2 = updown_sample(d3_2, 2)
+        d3to2_2 = self.convs[("conv1x1", 3, 2, 2)](d3to2_2)
+        d2_msf_2 = d3to2_2 + d2_2
 
-        d0_2_1 = self.convs[("conv1x1", 0, 2_1)](d0_2_1)
-        d0_3_2 = self.convs[("conv1x1", 0, 3_2)](d0_3_2)
-        d0_3_1 = self.convs[("conv1x1", 0, 3_1)](d0_3_1)
-        d0_4_3 = self.convs[("conv1x1", 0, 4_3)](d0_4_3)
-        d0_4_2 = self.convs[("conv1x1", 0, 4_2)](d0_4_2)
-        d0_4_1 = self.convs[("conv1x1", 0, 4_1)](d0_4_1)
+        d2_3 = self.convs[("parallel_conv", 2, 2)](d2_msf_2)
+        
+        d2to1_0 = updown_sample(d2_3, 2)
+        d2to1_0 = self.convs[("conv1x1", 2, 1, 0)](d2to1_0)
+        d1_msf_0 = d2to1_0 + d1_0
 
-        d0_1_msf = d0_1 + d0_2_1 + d0_3_1 + d0_4_1
-        d0_2_msf = d0_2 + d0_3_2 + d0_4_2
-        d0_3_msf = d0_3 + d0_4_3
+        d1_1 = self.convs[("parallel_conv", 1, 0)](d1_msf_0)
+        d1to0_0 = updown_sample(d1_1, 2)
+        d1to0_0 = self.convs[("conv1x1", 1, 0, 0)](d1to0_0)
+        
+        d0_0 = self.convs[("parallel_conv", 0, 0)](d1to0_0)
+        self.outputs[("disp", 0)] = self.sigmoid(self.convs[("dispconv", 0)](d0_0))
 
-
-        d1_1 = self.convs[("parallel_conv"), 1, 1](d0_1_msf)
-        d1_2 = self.convs[("parallel_conv"), 1, 2](d0_2_msf)
-        d1_3 = self.convs[("parallel_conv"), 1, 3](d0_3_msf)
-
-        d1_2_1 = updown_sample(d1_2, 2)
-        d1_3_2 = updown_sample(d1_3, 2)
-        d1_3_1 = updown_sample(d1_3, 4)
-
-        d1_2_1 = self.convs[("conv1x1", 1, 2_1)](d1_2_1)
-        d1_3_2 = self.convs[("conv1x1", 1, 3_2)](d1_3_2)
-        d1_3_1 = self.convs[("conv1x1", 1, 3_1)](d1_3_1)
-
-        d1_1_msf = d1_1 + d1_2_1 + d1_3_1
-        d1_2_msf = d1_2 + d1_3_2
-
-
-        d2_1 = self.convs[("parallel_conv"), 2, 1](d1_1_msf)
-        d2_2 = self.convs[("parallel_conv"), 2, 2](d1_2_msf)
-        d2_2_1 = updown_sample(d2_2, 2)
-        d2_2_1 = self.convs[("conv1x1", 2, 2_1)](d2_2_1)
-        d2_1_msf = d2_1 + d2_2_1
-
-
-        d3_0 = self.convs[("parallel_conv"), 3, 0](d0_0)
-        d3_1 = self.convs[("parallel_conv"), 3, 1](d2_1_msf)
-        d3_1_0 = updown_sample(d3_1, 2)
-        d3_1_0 = self.convs[("conv1x1", 3, 1_0)](d3_1_0)
-        d3_0_msf = d3_0 + d3_1_0
-
-
-        d4_0 = self.convs[("parallel_conv"), 4, 0](d3_0_msf)
-        d4_0 = updown_sample(d4_0, 2)
-        d5 = self.convs[("parallel_conv"), 5, 0](d4_0)
-        self.outputs[("disp", 0)] = self.sigmoid(self.convs[("dispconv", 0)](d5))
-
-            
         return self.outputs
     
